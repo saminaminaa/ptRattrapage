8a621c34646199b9ae3051164666fe7a5a5ef25d
noemie <noemie.lecherf@epsi.fr>
2021-04-01 12:15:26 +0000

    suite rattrapage

--- projet1/src/Controller/RattrapageController.php
+++ projet1/src/Controller/RattrapageController.php
@@ -6,20 +6,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\RattrapageType;
+use App\Entity\Rattrapage;
+use App\Entity\Utilisateur;
+use Symfony\Component\HttpFoundation\Request;
 
 class RattrapageController extends AbstractController
 {
     /**
      * @Route("/rattrapage", name="rattrapage")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $form = $this->createForm(ContactType::class);
+        $rattrapage = new Rattrapage();
+        $form = $this->createForm(RattrapageType::class,$rattrapage);
+        if ($request->isMethod('POST')) { 
+            $form->handleRequest($request); 
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($rattrapage);
+                $em->flush();
+                $this->addFlash('notice', 'Rattrapage inséré');
+            }
+            return $this->redirectToRoute('rattrapage');
+        } 
         return $this->render('rattrapage/index.html.twig', [
-            'form'=>$form->createView()
+        'form'=>$form->createView()
         ]);
-        //return $this->render('rattrapage/index.html.twig', [
-           // 'controller_name' => 'RattrapageController',
-        //]);
     }
 }
--- projet1/src/Entity/Rattrapage.php
+++ projet1/src/Entity/Rattrapage.php
@@ -22,7 +22,7 @@ class Rattrapage
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $PDF;
 
