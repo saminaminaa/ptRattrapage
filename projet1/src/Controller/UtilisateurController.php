7d32e53406e840d966ca4c6ce4184d67f4eb76a4
bayon <fabienbayon@yahoo.fr>
2021-04-01 13:27:42 +0000

    utilsateur et user

--- projet1/src/Controller/UtilisateurController.php
+++ projet1/src/Controller/UtilisateurController.php
@@ -6,28 +6,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\UtilisateurType;
-use App\Entity\Role;
 use App\Entity\Utilisateur;
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 
 class UtilisateurController extends AbstractController
 {
     /**
-     * @Route("/utilisateur", name="utilisateur")
+     * @Route("/ajoutUtilisateur/{id}", name="ajoutUtilisateur" , requirements={"id"="\d+"})
      */
-    public function index(Request $request): Response
+    public function ajoutUtilisateur(int $id,Request $request): Response
     {
         $utilisateur = new Utilisateur();
         $form = $this->createForm(UtilisateurType::class,$utilisateur);
+        $repoUser = $this->getDoctrine()->getRepository(User::class);
+        $user = $repoUser->find($id);
+        $utilisateur->setUser($user);
+
         if ($request->isMethod('POST')) { 
             $form->handleRequest($request); 
             if ($form->isSubmitted() && $form->isValid()) {
                 $em = $this->getDoctrine()->getManager();
                 $em->persist($utilisateur);
+                $user=$utilisateur->getUser();
+                $user->setUtilisateur($utilisateur);
+                $em->persist($user);
                 $em->flush();
                 $this->addFlash('notice', 'Utilisateur inséré');
             }
-            return $this->redirectToRoute('utilisateur');
+            return $this->redirectToRoute('accueil');
         } 
         return $this->render('utilisateur/index.html.twig', [
         'form'=>$form->createView()
