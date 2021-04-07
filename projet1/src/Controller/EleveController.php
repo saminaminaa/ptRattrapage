f95db2bf90417423081bf16a42fc822cf36eac13
noemie <noemie.lecherf@epsi.fr>
2021-04-07 07:12:27 +0000

    liste eleves

--- projet1/src/Controller/EleveController.php
+++ projet1/src/Controller/EleveController.php
@@ -32,4 +32,29 @@ class EleveController extends AbstractController
         'form'=>$form->createView()
         ]);
     }
+
+    /**
+    * @Route("/liste_eleves", name="liste_eleves")
+    */
+    public function listeEleves(Request $request)
+    {
+        $em = $this->getDoctrine();
+        $repoEleve = $em->getRepository(Eleve::class);
+
+        if ($request->get('supp')!=null){
+            $eleve = $repoEleve->find($request->get('supp'));
+            if($eleve!=null){
+                $em->getManager()->remove($eleve);
+                $em->getManager()->flush();
+            }
+            return $this->redirectToRoute('liste_eleves');
+        }
+           
+
+        $eleves = $repoEleve->findBy(array(),array('Nom'=>'ASC'));
+        
+        return $this->render('eleve/liste_eleve.html.twig', [
+        'eleves'=>$eleves 
+        ]);
+    }
 }
