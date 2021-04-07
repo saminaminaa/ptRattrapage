ac55e2826257c3ba90b1f506c6b33fd34309f69f
noemie <noemie.lecherf@epsi.fr>
2021-04-07 07:53:25 +0000

    liste modules rattrapages

--- projet1/src/Controller/ModuleRattrapageController.php
+++ projet1/src/Controller/ModuleRattrapageController.php
@@ -32,4 +32,29 @@ class ModuleRattrapageController extends AbstractController
         'form'=>$form->createView()
         ]);
     }
+
+        /**
+    * @Route("/liste_modules_rattrapages", name="liste_modules_rattrapages")
+    */
+    public function listeModulesRattrapages(Request $request)
+    {
+        $em = $this->getDoctrine();
+        $repoModulerattrapage = $em->getRepository(ModuleRattrapage::class);
+
+        if ($request->get('supp')!=null){
+            $modulerattrapage = $repoModulerattrapage->find($request->get('supp'));
+            if($rattrapage!=null){
+                $em->getManager()->remove($rattrapage);
+                $em->getManager()->flush();
+            }
+            return $this->redirectToRoute('liste_modules_rattrapages');
+        }
+           
+
+        $modulesrattrapages = $repoModulerattrapage->findBy(array(),array('libelle'=>'ASC'));
+        
+        return $this->render('module_rattrapage/liste_modules_rattrapages.html.twig', [
+        'modulesrattrapages'=>$modulesrattrapages
+        ]);
+    }
 }
